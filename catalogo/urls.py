cfe8f730233192e617083a7581717e84e9903337
EriVillalta <evillaltadelacruz@gmail.com>
2017-11-08 19:46:04 -0500

    proveedor 1

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -6,12 +6,9 @@ from .views.producto_view import ProductoViewSet
 from .views.compra_view import CompraViewSet
 from .views.venta_view import VentaViewSet
 from .views.detalleVenta_view import DetalleVentaViewSet
-<<<<<<< HEAD
 from .views.proveedor_view import ProveedorViewSet
-=======
 from .views.detalleCompra_view import DetalleCompraViewSet
 
->>>>>>> fbdd344289b5de519a11cab17033a6160685b4dc
 
 router = routers.DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
@@ -19,12 +16,10 @@ router.register(r'productos', ProductoViewSet)
 router.register(r'compras', CompraViewSet)
 router.register(r'ventas', VentaViewSet)
 router.register(r'detalleVenta', DetalleVentaViewSet)
-<<<<<<< HEAD
 router.register(r'proveedor', ProveedorViewSet)
-=======
 router.register(r'detalleCompra', DetalleCompraViewSet)
 
->>>>>>> fbdd344289b5de519a11cab17033a6160685b4dc
+
 
 urlpatterns = [
     url(r'^', include(router.urls)),
